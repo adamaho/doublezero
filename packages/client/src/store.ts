85a8d8aa6bed2035da09a9949abc6b447b381e59
adamaho <aho.adam01@gmail.com>
2023-06-20 10:28:01 -0400

    getting something super nice working

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -2,8 +2,31 @@ import jsonpatch from "fast-json-patch";
 import { IDBPDatabase, openDB } from "idb";
 import { makeObservable, observable, action } from "mobx";
 
-export async function createStoreDB(name: string, stores: { name: string; initialData: any }[]) {
+const data_suffix = "d";
+const patch_suffix = "p";
 
+/**
+ *
+ * stores
+ * {
+ *    count: {
+ *       push_url: "",
+ *       pull_url: "",
+ *       schema: zod schema
+ *       initia_data: {},
+ *       mutations: {
+ *          increment: (c) => {
+ *            return {
+ *               ...c,
+ *               count: c.count + 1
+ *            }
+ *          }
+ *       }
+ *    }
+ * }
+ */
+
+export async function createStoreDB(name: string, stores: { name: string; initialData: any }[]) {
   const db = await openDB(name, 1, {
     upgrade(db) {
       db.createObjectStore("store")  
@@ -14,7 +37,7 @@ export async function createStoreDB(name: string, stores: { name: string; initia
 
   const tx = db.transaction("store", "readwrite");
   for (const s of stores) {
-    const initialData = await tx.store.get(`${s.name}_d`) ?? s.initialData; 
+    const initialData = await tx.store.get(`${s.name}_${data_suffix}`) ?? s.initialData; 
     const store = new Store({ db, initialData, name: s.name });
     _stores[s.name] = store;
   }
@@ -45,8 +68,8 @@ export class Store {
   }
 
   private async commit(oldData: typeof this.data, newData: typeof this.data) {
-    const pKey = `${this._name}_p`;
-    const dKey = `${this._name}_d`;
+    const pKey = `${this._name}_${patch_suffix}`;
+    const dKey = `${this._name}_${data_suffix}`;
 
     const patch = jsonpatch.compare(oldData, newData);
     const tx = this._db.transaction("store", "readwrite");
